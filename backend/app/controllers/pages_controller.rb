b59d18d2ee3ca0e10cbb62e526e2a12e433cffba
davidnuna <david.nuna@takeofflabs.com>
2021-10-17 23:44:55 +0300

    Add seeds

--- backend/app/controllers/pages_controller.rb
+++ backend/app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   skip_authorization_check
 
   def home
-    render_response('Restaurant Backend is Online!')
+    render json: 'Restaurant Backend is Online!'
   end
 
 end
